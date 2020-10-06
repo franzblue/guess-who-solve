074624b06fe96c59feb73617c18b92506108c233
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-06 11:43:09 -0500

    got everything working, great job Casie

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 console.log('Here are all the available people:', people);
 
+let randomPerson = '';
 
 $(document).ready(onReady);
 
@@ -8,27 +9,48 @@ function onReady(){
     //append divs for all people
     appendPeople();
     pickRandomPerson();
+    $('#githubPeople').on('click', '.clickPic', clickedPerson)
 }
 
 function appendPeople(){
     people.forEach(function(person){
         console.log("person", person.name);
         $('#githubPeople').append(`
-            <li>
-                <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+            <li data-name=${person.name} class="clickPic">
+                <img src="https://github.com/${person.githubUsername}.png?size=250" 
+                alt="Profile image of ${person.name}">
             </li>
         `);
     })
-
 }
 
 
 function pickRandomPerson(){
     let randomNum = randomNumber(0, people.length - 1);
     console.log('random number', randomNum);
-    $('#person').append(people[randomNum].name);
+
+    randomPerson = people[randomNum].name;
+    $('#person').empty();
+    $('#person').append(randomPerson);
 }
 
 function randomNumber(min, max){
     return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function clickedPerson(){
+    let clickedPerson = $(this).data('name'); 
+    console.log('in clickedPerson', clickedPerson, randomPerson);
+
+    // compare the two names
+    // compare clickedPerson to random name
+
+    if( clickedPerson == randomPerson){
+        alert (`yes!! this is ${randomPerson}`)
+
+        pickRandomPerson();
+    }
+    else{
+        alert ('nope, try again')
+    }
 }
